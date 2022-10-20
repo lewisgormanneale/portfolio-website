58912deb67b2cdf83ba106c2384d04ef5ec48b83
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2022-10-20 19:35:44 +0100

    dark mode siwtch styling, typing effect progress

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-console.log("Hello World!")
+function darkModeSwitch() {
+    console.log('dark mode switch')
+}
 
 function reveal() {
     
 }
+
+let i = 0;
+let txt = `console.log('Hello World!');`; /* The text */
+let speed = 100; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+    let heading = document.getElementById("demo")
+    heading.classList.add("typewriter")
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+typeWriter()
