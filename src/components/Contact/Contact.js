348da3a5ddb37681a3d62a8f532a8413aaeea336
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2022-12-19 12:33:20 +0000

    better social icon styling, removed contact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -11,52 +11,14 @@ export default function Contact() {
           <p className="contact-greeting">
             I'm enrolled on the School of Code until February 2023, at which
             point I'll be looking to start my career as a developer. If you'd
-            like to discuss a potential oppurtunity or anything else, just reach
-            out through one of the methods here!
+            like to discuss a potential opportunity or anything else, just reach
+            out to me on one of my socials!
           </p>
           <div className="social-icons">
-            <IoLogoLinkedin />
-            <IoLogoGithub />
-            <IoLogoTwitter />
+            <IoLogoLinkedin className="social-icon" id="linkedin" />
+            <IoLogoGithub className="social-icon" id="github" />
+            <IoLogoTwitter className="social-icon" id="twitter" />
           </div>
-        </div>
-        <div className="contact-form-container">
-          <form name="contact-form" className="contact-form">
-            <label className="hidden" htmlFor="name">
-              name
-            </label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              placeholder="Name"
-              required
-            />
-            <label className="hidden" htmlFor="email">
-              email
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              placeholder="Email"
-              required
-            />
-            <label className="hidden" htmlFor="message">
-              message
-            </label>
-            <textarea
-              name="message"
-              id="message"
-              placeholder="Message"
-              required
-            ></textarea>
-            <div className="submit-container">
-              <button className="send-button" type="submit">
-                Send Message
-              </button>
-            </div>
-          </form>
         </div>
       </div>
     </section>
